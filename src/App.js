19abf271ddb67806e5328dbb006275bca5f5b672
Roadm11 <rodrigo.adm10@hotmail.com>
2020-07-10 02:16:21 -0300

    Repositories FronEnd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
-  }
+    const response = await api.post('/repositories', {
+      title: `Repositório ${Date.now()}`,
+      url: `http://github.com/Roadm11/${Date.now()}`,
+      techs: "Node Js, React Native, C#"
+    });
+    const repository = response.data;
+    
+    setRepositories([...repositories, repository]);
+  };
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    await api.delete(`/repositories/${id}`);
+    const repoFiltered = repositories.filter(repo => repo.id !== id);
+
+    setRepositories(repoFiltered);
+  };
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
